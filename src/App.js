3e0f406a340e24edb6796b402f719b6183f48f27
Iouri Volkov <volk.iouri@gmail.com>
2022-08-03 18:26:13 -0400

    Add Navigation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
+
+const title = "CNTKT";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navigation title={title} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,37 @@
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
+
+const Navigation = ({ title }) => {
+  return (
+    <Box>
+      <AppBar
+        position="static"
+        color="inherit"
+        sx={{
+          boxShadow: "none",
+          height: "80px",
+          display: "flex",
+          justifyContent: "center",
+          marginLeft: "2em",
+        }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              flexGrow: 1,
+              fontFamily: "Futura",
+              fontWeight: "bold",
+              fontSize: "1.7em",
+              color: "black",
+            }}
+          >
+            {title}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default Navigation;
